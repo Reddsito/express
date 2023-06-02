32b985a5ba0354863d8cd4d8bfb698696ff096f1
Reddsito <ptyelnegro@gmail.com>
2023-06-02 05:23:09 -0500

    modificaciones

--- api/src/controllers/products.controller.js
+++ api/src/controllers/products.controller.js
@@ -4,29 +4,33 @@ import { ProductCategory } from '../models/ProductCategory.js';
 
 export const getProjects = async (req, res) => {
   try {
-    const { query: name } = req.query;
+    const { name, sort } = req.query;
     let products;
 
-    console.log(name)
+    const whereClause = {};
 
+    if (name && name.length > 0) {
+      whereClause.name = {
+        [Sequelize.Op.like]: `%${name}%`
+      };
+    }
 
-    if ( name && name?.length > 0) {
-      console.log('entro aqui')
-      // Si se proporcionan queries de filtro, realizar la consulta filtrada
-      products = await Product.findAll({
-        where: {
-          name: {
-            [Sequelize.Op.like]: `%${name}%`
-          }
-        }
-      });
+    const orderClause = [];
+
+    if (sort === 'priceDesc') {
+      orderClause.push(['price', 'DESC']);
+    } else if (sort === 'priceAsc') {
+      orderClause.push(['price', 'ASC']);
+    } else if (sort === 'name') {
+      orderClause.push(['name', 'ASC']);
     } 
 
-    if (!name) {
-        // Si no se proporcionan queries de filtro, obtener todos los productos
-        products = await Product.findAll();
-      
-    }
+    console.log({orderClause})
+
+    products = await Product.findAll({
+      where: whereClause,
+      order: orderClause
+    });
 
     res.json(products);
   } catch (error) {
